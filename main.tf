433da100c753d30dc071dd39289691a7c53b5b68
jr9648 <jr9648@gmail.com>
2023-09-27 13:57:49 +0530

    terraform files

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
   access_key = "<enter your access key here>"
-  secret_key = "enter your secret key here"
+  secret_key = "<enter your secret key here>"
 }
 
 module "ec2_instances" {
