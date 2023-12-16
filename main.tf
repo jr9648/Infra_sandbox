e8b7d4936a9cc63f6d316552f69f78546027ddec
jr9648 <jr9648@gmail.com>
2023-12-16 22:23:05 +0530

    modules created

--- main.tf
+++ main.tf
@@ -1,21 +1,9 @@
-data "external" "load_env" {
-  program = ["sh", "-c", "source .env && env"]
-}
-
-locals {
-  aws_access_key = try(jsondecode(data.external.load_env.result)["AWS_ACCESS_KEY_ID"], "")
-  aws_secret_key = try(jsondecode(data.external.load_env.result)["AWS_SECRET_ACCESS_KEY"], "")
-}
-
-
 provider "aws" {
-  region     = "us-east-1"
-  access_key = local.aws_access_key
-  secret_key = local.aws_secret_key
+  region     = "ap-south-1"
 }
 
-module "ec2_instances" {
-  source = "./module/ec2-instances"  # Update this path to point to your module directory.
+module "instances" {
+  source = "./instances"  # Update this path to point to your module directory.
 
   frontend_ami_id     = "ami-053b0d53c279acc90"
   backend_ami_id      = "ami-053b0d53c279acc90"
@@ -24,4 +12,12 @@ module "ec2_instances" {
   backend_instance_type  = "t2.micro"  # Set the instance type for the backend instance
   database_instance_type = "t2.micro"  # Set the instance type for the database instance
   
+}
+
+module "sg" {
+  source = "./sg"
+}
+
+module "ssh" {
+  source = "./ssh-key"
 }
